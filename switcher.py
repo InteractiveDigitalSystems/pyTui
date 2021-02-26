1712e0ee8a225c73a961ec1c9f4c5d50a69652d7
Ebbe Vang <ev@evang.dk>
2021-02-27 00:04:39 +0100

    fixed switcher.py

--- switcher.py
+++ switcher.py
@@ -5,32 +5,31 @@ class App(npyscreen.NPSAppManaged):
         #add forms to the application
         self.addForm('MAIN', FirstForm, name="main")
         self.addForm('SECOND', SecondForm, name="second")
-    def change_form(self):
-        self.switchForm('SECOND')
+    def change_form(self, name):
+        self.switchForm(name)
         self.resetHistory()
       
-class FirstForm(npyscreen.Form):
+class FirstForm(npyscreen.ActionFormMinimal):
     def create(self):
         self.add(npyscreen.TitleText, name = "Text:", value= "Press ^T to change screen" )
         self.add(Selector, id="selector", name="Select", values=['first', 'second'], relx=3)
         self.add_handlers({"^T": self.switch})
 
     def switch(self, *args, **keywords):
-        self.parentApp.change_form()
+        self.parentApp.change_form(None)
 
-    def afterEditing(self):
-        self.parentApp.setNextForm(None)
-
-class SecondForm(npyscreen.Form):
+class SecondForm(npyscreen.ActionFormMinimal):
     def create(self):
         self.add(npyscreen.TitleText, name = "Text:", value= "Press ^T to change screens" )
     
-    def afterEditing(self):
-        self.parentApp.setNextForm(None)
+    def on_ok(self):
+        self.parentApp.change_form(None)
 
 class Selector(npyscreen.MultiLineAction):
+
     def actionHighlighted(self, act_on_this, key_press):
-        self.parent.parentApp.change_form()
+        self.parent.parentApp.change_form('SECOND')
+
 
 app = App()
 app.run()
