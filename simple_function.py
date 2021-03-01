a2436ea6909e31f60482a5446dd981d55563dfd6
Ebbe Vang <ev@evang.dk>
2021-03-01 10:41:51 +0100

    cleaned repo

--- simple_function.py
+++
@@ -1,7 +0,0 @@
-import npyscreen
-
-def simple_function ( *arguments):
-    form = npyscreen.Form( name='hello world', lines=6, columns=40)
-    form.edit()
-
-npyscreen.wrapper_basic(simple_function)
